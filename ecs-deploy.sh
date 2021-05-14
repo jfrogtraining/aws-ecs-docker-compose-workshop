eb59db761a0b958c82dc8f998b354cc88f06a5ae
Jeff Fry <jeff@jefferyfry.com>
2021-05-14 14:16:08 -0700

    Updated with docker compose deployment and new images.

---
+++ ecs-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt  install jq -y
+curl -L -o docker-linux-amd64.tar.gz https://github.com/docker/compose-cli/releases/download/v1.0.10/docker-linux-amd64.tar.gz
+tar xzf docker-linux-amd64.tar.gz
+chmod +x docker/docker
+which docker
+sudo ln -s $(which docker) /usr/local/bin/com.docker.cli
+./docker/docker --context default ps
+sudo mv docker/docker /usr/local/bin/docker
+docker version
+
+printenv
+
+docker context create ecs aws-ecs-docker-compose-workshop --from-env
+
+docker compose -f docker-compose.yaml -f  docker-compose.prod.migrate.yaml -f docker-compose.prod.scaling.yaml up
