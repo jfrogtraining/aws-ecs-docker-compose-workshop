c78c1f035e7172d2971050d04b554c0736c58be3
Jeff Fry <jeff@jefferyfry.com>
2021-05-14 19:46:19 -0700

    Updates.

--- ecs-deploy.sh
+++ ecs-deploy.sh
@@ -12,6 +12,6 @@ docker version
 
 docker context create ecs aws-ecs-docker-compose-workshop --from-env
 
-docker context use aws-ecs-docker-compose-workshop --from-env
+docker context use aws-ecs-docker-compose-workshop
 
-docker compose -f docker-compose.yaml -f  docker-compose.prod.migrate.yaml -f docker-compose.prod.scaling.yaml up -d
+docker compose -f docker-compose.yaml -f  docker-compose.prod.migrate.yaml -f docker-compose.prod.scaling.yaml up -d --verbose
