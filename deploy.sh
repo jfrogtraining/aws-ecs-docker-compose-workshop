bc2e1db24ad95592ecf5eaf1cd6bc5ab49ea2dfb
Jeff Fry <jeff@jefferyfry.com>
2021-05-14 21:44:57 -0700

    Updates.

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,6 @@ sudo ln -s $(which docker) /usr/local/bin/com.docker.cli
 ./docker/docker --context default ps
 sudo mv docker/docker /usr/local/bin/docker
 docker version
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
 docker context create ecs ecsDocker --from-env
 docker context use ecsDocker
 docker compose -f docker-compose.yaml -f  docker-compose.prod.migrate.yaml -f docker-compose.prod.scaling.yaml up -d
