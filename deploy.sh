f4d94ea68f64b0d7279e3e522a48e30bd2ad8ce5
Jeff Fry <jeff@jefferyfry.com>
2021-05-17 16:59:31 -0700

    Fixed wait loop.

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ docker version
 docker context create ecs ecsDocker${run_number} --from-env
 docker context use ecsDocker${run_number}
 docker compose -f docker-compose.yaml up
-while [ -z `docker-compose ps -q app` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q app)` ]; do sleep 2; done
+until [ "`docker inspect -f {{.State.Running}} app`"=="true" ]; do sleep 1; done;
 echo "App is ready!"
 docker compose ps
 #docker compose -f docker-compose.yaml convert > aws-cloudformation.yaml
