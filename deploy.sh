b96815dbd619760a890473e103080c81dfb45b8a
Jeff Fry <jeff@jefferyfry.com>
2021-05-17 18:33:38 -0700

    Added debug

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,9 @@ docker version
 docker context create ecs ecsDocker${run_number} --from-env
 docker context use ecsDocker${run_number}
 docker compose -f docker-compose.yaml up
-until [ "`docker inspect --format={{.State.Running}} app`"=="true" ]; do sleep 1; done;
+docker ps
+docker inspect app
+until [ "docker inspect --format='{{.State.Running}}' app"]; do sleep 1; done;
 echo "App is ready!"
 docker compose ps
 #docker compose -f docker-compose.yaml convert > aws-cloudformation.yaml
