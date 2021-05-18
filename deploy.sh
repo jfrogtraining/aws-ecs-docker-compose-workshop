4e3ba3a5278117039bd32c35423990a860975452
Jeff Fry <jeff@jefferyfry.com>
2021-05-17 18:55:01 -0700

    Fixed typo.

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ docker version
 docker context create ecs ecsDocker${run_number} --from-env
 docker context use ecsDocker${run_number}
 docker compose -f docker-compose.yaml up
-while [ -z "docker ps --filter status=running | grep app"]; do sleep 1; done;
+while [ -z "docker ps --filter status=running | grep app" ]; do sleep 1; done;
 echo "App is ready!"
 docker compose ps
 #docker compose -f docker-compose.yaml convert > aws-cloudformation.yaml
